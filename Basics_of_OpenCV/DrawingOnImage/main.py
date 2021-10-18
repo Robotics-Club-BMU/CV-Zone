adc28602c9c543ae5b594d87a725b25a26ae8f2a
Anmol Jhamb <codingwithaj@gmail.com>
2021-10-18 10:18:21 +0530

    changed the code to have all the drawings in different variables

--- Basics_of_OpenCV/DrawingOnImage/main.py
+++ Basics_of_OpenCV/DrawingOnImage/main.py
@@ -4,18 +4,28 @@ import numpy as np
 
 def main():
     blank_image = np.zeros((500, 500), dtype="uint8")
-
     cv.rectangle(blank_image, (0, 0), (150, 200), (255, 255, 255), thickness=5)
+    cv.imshow("Rectangle", blank_image)
+    cv.imwrite("rectangle.jpg", blank_image)
+
+    blank_image = np.zeros((500, 500), dtype="uint8")
     cv.circle(blank_image, (0, 500), 200, (255, 255, 255), thickness=-1)
+    cv.imshow("Circle", blank_image)
+    cv.imwrite("circle.jpg", blank_image)
+
+    blank_image = np.zeros((500, 500), dtype="uint8")
     cv.line(blank_image, (0, 500), (500, 0), (255, 255, 255), thickness=5)
+    cv.imshow("Line", blank_image)
+    cv.imwrite("line.jpg", blank_image)
 
     triangle = np.array([[[350, 130], [380, 230], [190, 280]]], np.int32)
+    blank_image = np.zeros((500, 500), dtype="uint8")
     cv.polylines(blank_image, [triangle], True, (255,255,255), thickness=8)
-    cv.imshow("Cirlce", blank_image)
+    cv.imshow("Polygon", blank_image)
+    cv.imwrite("polygon.jpg", blank_image)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
-    cv.imwrite("output.jpg", blank_image)
 
 
 if __name__ == "__main__":
