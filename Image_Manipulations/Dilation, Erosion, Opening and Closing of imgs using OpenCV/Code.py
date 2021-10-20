82c98680bf9a9e19c47d3e242742d5039712bf7e
anou1234 <92670920+anou1234@users.noreply.github.com>
2021-10-20 16:31:39 +0530

    Update Code.py

--- Image_Manipulations/Dilation, Erosion, Opening and Closing of imgs using OpenCV/Code.py
+++ Image_Manipulations/Dilation, Erosion, Opening and Closing of imgs using OpenCV/Code.py
@@ -16,7 +16,6 @@ kernel = np.ones((5,5), np.uint8)
 # you want to erode/dilate a given image.
 img_erosion = cv2.erode(img, kernel, iterations=1)
 img_dilation = cv2.dilate(img, kernel, iterations=1)
- 
 cv2.imshow('Input', img)
 cv2.imshow('Dilation', img_dilation)
 cv2.imshow('Erosion', img_erosion)
