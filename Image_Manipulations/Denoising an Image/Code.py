1a9358fb44146f28824a852e585f62ba9d3ba7b4
Tanishq Bhatt <58645688+tb-rules10@users.noreply.github.com>
2021-10-20 20:39:55 +0530

    Create Code.py

---
+++ Image_Manipulations/Denoising an Image/Code.py
@@ -0,0 +1,18 @@
+# Denoising an Image
+
+# importing libraries
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+# Reading image from folder where it is stored
+img = cv2.imread('noise1.jpg')
+
+# denoising of image saving it into dst image
+dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
+
+# Plotting of source and destination image
+plt.subplot(121), plt.imshow(img)
+plt.subplot(122), plt.imshow(dst)
+
+plt.show()
